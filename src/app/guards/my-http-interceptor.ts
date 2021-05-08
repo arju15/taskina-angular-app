a00020cd66ce79799d5ae456ab91e767bc7e9cbe
arju15 <arzuu.1501@gmail.com>
2021-05-08 20:04:23 +0530

    taskina initial commit

---
+++ src/app/guards/my-http-interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class MyHttpInterceptor implements HttpInterceptor {
+    constructor(private router: Router) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        const userToken = JSON.parse(localStorage.getItem('userToken'));
+        let authReq = req.clone();
+
+        if (userToken) {
+            // Clone the request to add the new header.
+            authReq = req.clone({ headers: req.headers.set("authorization", "bearer " + userToken.access_token) });
+
+            console.log("Sending request with new header now ...");
+        }
+
+        //send the newly created request
+        return next.handle(authReq)   
+            .catch((error, caught) => {
+                if (error.status === 401) {
+                    localStorage.removeItem('userToken');
+                    this.router.navigate(['login']);
+                }
+                //return the error to the method that called it
+                return Observable.throw(error);
+            }) as any;
+    }
+}
